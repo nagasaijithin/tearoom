9bee8e04f79424b4a8fdf620b4688bf98d2531f3
nagasaijithin <nagasaijithin1@gmail.com>
2021-04-29 12:13:05 +0530

    first commit

--- js/custom.js
+++ js/custom.js
@@ -6,24 +6,25 @@ const email = document.querySelector("#email-form5-u");
 const phonenumber = document.querySelector("#phone-form5-u");
 const message = document.querySelector("#textarea-form5-u");
 
-console.log(formwapper);
-formwapper.addEventListener("submit", (e) => {
-  e.preventDefault();
-  emailjs
-    .send("service_qnet4lm", "template_ov1kgvb", {
-      from_name: "Tea Room Website",
-      to_name: fullname.value,
-      to_phone: phonenumber.value,
-      to_email: email.value,
-      to_message: message.value,
-    })
-    .then((e) => {
-      fullname.value = "";
-      phonenumber.value = "";
-      email.value = "";
-      message.value = "";
-    });
-});
+// console.log(formwapper);
+formwapper &&
+  formwapper.addEventListener("submit", (e) => {
+    e.preventDefault();
+    emailjs
+      .send("service_qnet4lm", "template_ov1kgvb", {
+        from_name: "Tea Room Website",
+        to_name: fullname.value,
+        to_phone: phonenumber.value,
+        to_email: email.value,
+        to_message: message.value,
+      })
+      .then((e) => {
+        fullname.value = "";
+        phonenumber.value = "";
+        email.value = "";
+        message.value = "";
+      });
+  });
 
 const scrollTopBtn = document.querySelector(".scroll-top-btn");
 window.onscroll = function () {
@@ -31,6 +32,7 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
+  console.log("hello");
   if (
     document.body.scrollTop > 100 ||
     document.documentElement.scrollTop > 100
